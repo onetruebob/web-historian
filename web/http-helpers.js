99eeb53f43dead574df89dac0c817a70c6c7a0c3
Hack Reactor Students <bob@bobowen.org>
2014-06-25 15:29:35 -0700

    Handles serving base HTML

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -11,8 +11,23 @@ exports.headers = headers = {
 };
 
 exports.serveAssets = function(res, asset) {
+  var assetPath = archive.paths.siteAssets + asset;
+
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  fs.readFile(assetPath, function(error, data){
+    if (error){
+      // 404 not found helper
+      notFoundHelper(res);
+    }
+    res.writeHead(200, headers);
+    res.end(data);
+  });
+};
+
+exports.notFoundHelper = notFoundHelper = function(res) {
+  res.writeHead(404, headers);
+  res.end("File Not Found");
 };
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,25 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers.js');
 // require more modules/folders here!
 
+
+var getHandler = function(req, res){
+  var requestedFile = req.url.split("?")[0];
+
+  if(requestedFile === '/'){
+    requestedFile = '/index.html';
+  }
+
+  httpHelpers.serveAssets(res, requestedFile);
+};
+
+var statusRouter = {
+  "GET": getHandler,
+  //"POST": postHandler,
+  //"OPTIONS": optionsHandler
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  statusRouter[req.method](req, res);
 };
